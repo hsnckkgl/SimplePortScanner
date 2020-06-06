6f2d2fea211d4b811841fb6dc102c43ba6f7b7c0
Hasan Cokakoglu <hsnckkgl@gmail.com>
2020-06-06 01:27:20 +0100

    end

--- SimplePortScanner.py
+++ SimplePortScanner.py
@@ -1,36 +1,53 @@
-import sys
+#!/usr/bin/env python
 import socket
+import subprocess
+import sys
 from datetime import datetime
 
-# Target definition
-if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1]) # Hostname translation to IPv4
-else:
-    print("Invalid amount of argument!")
-    print("Syntax: python3 SimplePortScanner.py <IP>")
+# Clear the screen
+subprocess.call('clear', shell=True)
 
-print("#" * 60)
-print("Scanning target " + target)
-print("Time Started: " + str(datetime.now()))
-print("#" * 60)
+# Ask for input
+remoteServer=input("Enter a remote host to scan: ")
+remoteServerIP=socket.gethostbyname(remoteServer)
+
+# Print a nice banner with information on which host we are about to scan
+print("-" * 60)
+print("Please wait, scanning remote host", remoteServerIP)
+print("-" * 60)
+
+# Check what time the scan started
+t1 = datetime.now()
+
+# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+
+# We also put in some error handling for catching errors
 
 try:
-    for port in range(1,65535):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.5)
-        result = s.connect_ex((target, port))
+    for port in range(1,1025):  
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
-            print("Port {} is open.".format(port))
-        s.close()
+            print("Port {}: 	 Open".format(port))
+        sock.close()
 
 except KeyboardInterrupt:
-    print("\nExiting program!")
+    print("You pressed Ctrl+C")
     sys.exit()
 
 except socket.gaierror:
-    print("Hostname could not be resolved!")
+    print('Hostname could not be resolved. Exiting')
     sys.exit()
 
 except socket.error:
-    print("Could not connect to server!")
+    print("Couldn't connect to server")
     sys.exit()
+
+# Checking the time again
+t2 = datetime.now()
+
+# Calculates the difference of time, to see how long it took to run the script
+total =  t2 - t1
+
+# Printing the information to screen
+print('Scanning Completed in: ', total)
