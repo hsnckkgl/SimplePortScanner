61d7c298f8c450d650b936402da484041da1214f
Hasan Cokakoglu <hsnckkgl@gmail.com>
2020-06-06 01:38:41 +0100

    finish

--- SimplePortScanner.py
+++ SimplePortScanner.py
@@ -11,15 +11,13 @@ subprocess.call('clear', shell=True)
 remoteServer=input("Enter a remote host to scan: ")
 remoteServerIP=socket.gethostbyname(remoteServer)
 
-# Print a nice banner with information on which host we are about to scan
-print("-" * 60)
-print("Please wait, scanning remote host", remoteServerIP)
-print("-" * 60)
+# Banner
+print("+" * 60)
+print("Scanning remote host", remoteServerIP)
+print("+" * 60)
 
 # Check what time the scan started
 t1 = datetime.now()
-
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
 
 # We also put in some error handling for catching errors
 
@@ -28,19 +26,19 @@ try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
-            print("Port {}: 	 Open".format(port))
+            print("Port {}: Open".format(port))
         sock.close()
 
 except KeyboardInterrupt:
-    print("You pressed Ctrl+C")
+    print("You aborted the scan!")
     sys.exit()
 
 except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
+    print('Hostname could not be resolved!')
     sys.exit()
 
 except socket.error:
-    print("Couldn't connect to server")
+    print("Couldn't connect to server!")
     sys.exit()
 
 # Checking the time again
